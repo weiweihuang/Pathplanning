55bb4e7b35e42d6e0f2784eae74e0228ebf3557f
weiweihuang <weiweihuang@weiweihuang-i2r.(none)>
2014-01-16 13:15:25 +0800

    with new car model

--- car_drive_control/src/auto_car_control.cpp
+++ car_drive_control/src/auto_car_control.cpp
@@ -72,9 +72,11 @@ float K_d = 5.0;
 
 float ref_traj[2][SIM_TIME];	
 float ori_traj[3][SIM_TIME];
-float log_data[12][SIM_TIME]; //ref(x,y,a), actual(x,y,a), ref(v,steeling), actual(v,steeling),gaspedal, breakpedal
+float log_data[13][SIM_TIME]; //ref(x,y,a), actual(x,y,a), ref(v,steeling), actual(v,steeling),gaspedal, breakpedal, direction
+float test_data[4][SIM_TIME]; 
 
 float cur_x, cur_y, cur_vel, cur_ori, gas_pedal_state, break_pedal_state;
+int direction_state;
 
 static void quit(int sig)
 {
@@ -86,10 +88,27 @@ static void quit(int sig)
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
 
-   cur_vel = sqrtf(msg->twist.twist.linear.x*msg->twist.twist.linear.x+msg->twist.twist.linear.y*msg->twist.twist.linear.y);
-   cur_ori = tf::getYaw(msg->pose.pose.orientation);
-   cur_x = msg->pose.pose.position.x;
-   cur_y = msg->pose.pose.position.y;			
+   static int count = 0;
+   static float ini_pos_x=0, ini_pos_y=0, ini_angle=0;	
+   if (count == 0){
+	ini_pos_x = msg->pose.pose.position.x;
+	ini_pos_y = msg->pose.pose.position.y;
+	cur_vel = sqrtf(msg->twist.twist.linear.x*msg->twist.twist.linear.x+msg->twist.twist.linear.y*msg->twist.twist.linear.y);
+	cur_ori = 0;
+	cur_x = 0;
+	cur_y = 0;
+	count++;
+   }
+   else{
+	cur_vel = sqrtf(msg->twist.twist.linear.x*msg->twist.twist.linear.x+msg->twist.twist.linear.y*msg->twist.twist.linear.y);
+	cur_ori = (tf::getYaw(msg->pose.pose.orientation)+3.14159);
+	if (cur_ori > 3.14159)
+		cur_ori = cur_ori - 2* 3.14159;
+	cur_x = msg->pose.pose.position.x - ini_pos_x;
+	cur_y = msg->pose.pose.position.y - ini_pos_y;
+   }		
+
+			
 
 /*
    std_msgs::Float64 gasPedal, handWheel, breakPedal;
@@ -165,12 +184,19 @@ void breakPedalCallback(const std_msgs::Float64::ConstPtr& msg)
 	break_pedal_state = msg->data;
 }
 
+void directionCallback(const std_msgs::Int8::ConstPtr& msg)
+{
+	
+	direction_state = msg->data;
+}
+
 void wheelCallback(const std_msgs::Float64::ConstPtr& msg)
 {
 
 	std_msgs::Float64 gasPedal, handWheel, breakPedal;
 	std_msgs::Int8 direction;
 	static int count = 0;
+	static int save_flag = 0;
 	float cur_wheel = msg->data;
         float vel_error;
 	float k_d = 5.0, k_p = 0.1;
@@ -194,22 +220,56 @@ void wheelCallback(const std_msgs::Float64::ConstPtr& msg)
 			log_data[9][count] = cur_wheel;
 			log_data[10][count] = gas_pedal_state;
 			log_data[11][count] = break_pedal_state;
+			log_data[12][count] = direction_state;
+			test_data[0][count] = 0;
+			test_data[1][count] = 0;
+			test_data[2][count] = 0;
+			test_data[3][count] = 0;			
 
 
 			count++;
 		}
 		else{
-			vel_error = ref_traj[0][count] - cur_vel;
+
+			//change current and reference position to car coordinate 
+			float ref_car_x = ori_traj[0][count]*cosf(cur_ori) + ori_traj[1][count]*sinf(cur_ori);
+			float ref_car_y = -ori_traj[0][count]*sinf(cur_ori) + ori_traj[1][count]*cosf(cur_ori);
+			float car_x = cur_x*cosf(cur_ori) + cur_y*sinf(cur_ori);
+			float car_y = -cur_x*sinf(cur_ori) + cur_y*cosf(cur_ori);
+			
+
+
+			vel_error = ref_traj[0][count] - cur_vel + 0.25*(ref_car_x - car_x);
+
 			if (vel_error>0){
 				gasPedal.data = 5.0*vel_error-0.5*gas_pedal_state;
-				breakPedal.data = 0;	
+				breakPedal.data = 0.0;
 			}
 			else{
-				breakPedal.data = 10.0*vel_error;
-				gasPedal.data = 0;	
+				gasPedal.data = 0;
+				breakPedal.data = -10.0*vel_error;
 			}
 
-			handWheel.data = 3.0*ref_traj[1][count] - 2.0*(cur_ori - ori_traj[2][count]);
+
+/*
+
+			gasPedal.data = 5.0*vel_error-0.5*gas_pedal_state + 0.0*(ref_car_x - car_x);
+
+		        if (gasPedal.data < 0){
+				gasPedal.data = -gasPedal.data;
+				direction.data = -1;
+			}
+			else
+				direction.data = 1;
+
+
+			if (ref_car_x - car_x<0)
+				breakPedal.data = -0.1*(ref_car_x - car_x);
+			else
+				breakPedal.data = 0.0;
+*/
+
+			handWheel.data = 5.0*ref_traj[1][count] - 2.0*(cur_ori - ori_traj[2][count]) + 0.5*(ref_car_y - car_y);
 
 			log_data[0][count] = ori_traj[0][count];
 			log_data[1][count] = ori_traj[1][count];
@@ -223,20 +283,26 @@ void wheelCallback(const std_msgs::Float64::ConstPtr& msg)
 			log_data[9][count] = cur_wheel;
 			log_data[10][count] = gas_pedal_state;
 			log_data[11][count] = break_pedal_state;
+			log_data[12][count] = direction_state;
+			test_data[0][count] = ref_car_x;
+			test_data[1][count] = ref_car_y;
+			test_data[2][count] = car_x;
+			test_data[3][count] = car_y;
 
 			count++;
-			if (count>SIM_TIME-1)
+			if (count>SIM_TIME-1){
 				count = SIM_TIME-1;
+				ROS_INFO( "test finished");
+			}			
 		
 		}
-		//ROS_INFO( "gaspedal command is %g, handWheel command is %g, and breakpedal command is %g", gasPedal.data, handWheel.data, breakPedal.data);
-		//ROS_INFO( "cycle number %d, desired vel is %g, and actual vel is %g", count, ref_traj[0][count], cur_vel);
-		//ROS_INFO( "ref and acutal wheel are %g %g, ref and actual orientation are %g %g", ref_traj[1][count], cur_wheel, ori_traj[2][count], cur_ori);
+
 		car_gasPedal_pub_.publish(gasPedal); 
 		car_handWheel_pub_.publish(handWheel); 
 		car_breakPedal_pub_.publish(breakPedal);	
 	}
-	else if (carStop && count > 1000){
+	else if (carStop && count > 100 && save_flag==0){
+
 		FILE *my_stream;
 
 		//save data
@@ -247,10 +313,25 @@ void wheelCallback(const std_msgs::Float64::ConstPtr& msg)
 			exit(1);
 		}
 		for (int i=0; i<SIM_TIME;i++){
-			fprintf(my_stream,"%.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f\n", log_data[0][i],log_data[1][i],log_data[2][i],log_data[3][i],log_data[4][i],log_data[5][i],log_data[6][i],log_data[7][i],log_data[8][i],log_data[9][i],log_data[10][i],log_data[11][i]);
+			fprintf(my_stream,"%.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f\n", log_data[0][i],log_data[1][i],log_data[2][i],log_data[3][i],log_data[4][i],log_data[5][i],log_data[6][i],log_data[7][i],log_data[8][i],log_data[9][i],log_data[10][i],log_data[11][i],log_data[12][i]);
 		}
 		fclose(my_stream);
+		// save test data
+		FILE *my_test;
+
+		//save data
+		my_test=fopen("/home/weiweihuang/Matlab/Car_Traj/test_log.txt", "w");
+		if (my_test==NULL){
+			printf("Cannot open the file");
+			fclose(my_test);
+			exit(1);
+		}
+		for (int i=0; i<SIM_TIME;i++){
+			fprintf(my_test,"%.5f %.5f %.5f %.5f\n", test_data[0][i],test_data[1][i],test_data[2][i],test_data[3][i]);
+		}
+		fclose(my_test);
 		ROS_INFO( "Log data saved");
+		save_flag=1;
 
 	}
 
@@ -325,7 +406,9 @@ int main(int argc, char **argv)
     ros::Subscriber sub_odom_ = nh.subscribe<nav_msgs::Odometry>("/car_truth_odom", 100, &odomCallback);
     ros::Subscriber sub_wheel_ = nh.subscribe<std_msgs::Float64>("/drc_vehicle/hand_wheel/state", 100, &wheelCallback);
     ros::Subscriber sub_gas_pedal_ = nh.subscribe<std_msgs::Float64>("/drc_vehicle/gas_pedal/state", 100, &gasPedalCallback);
-    ros::Subscriber sub_break_pedal_ = nh.subscribe<std_msgs::Float64>("/drc_vehicle/break_pedal/state", 100, &breakPedalCallback);		
+    ros::Subscriber sub_break_pedal_ = nh.subscribe<std_msgs::Float64>("/drc_vehicle/break_pedal/state", 100, &breakPedalCallback);
+    ros::Subscriber sub_direction_ = nh.subscribe<std_msgs::Int8>("/drc_vehicle/direction/state", 100, &directionCallback);	
+	
 
 
     car_handBrake_pub_ = nh.advertise<std_msgs::Float64>("/drc_vehicle/hand_brake/cmd", 100);	
@@ -335,20 +418,36 @@ int main(int argc, char **argv)
     car_direction_pub_ = nh.advertise<std_msgs::Int8> ("/drc_vehicle/direction/cmd", 100);
 
     // initial trajectory
+    float Degg = 3.14159/3.0f;
+    float RR = 2.0f/sinf(Degg);	
    
    for (int i=0; i< SIM_TIME; i++){
-	if(i<1000){
+	if(i<400){
 		ori_traj[0][i] = 0.02*(i+1);
 		ori_traj[1][i] = 0.0;
+		ori_traj[2][i] = 0.0;
+		//if (i>350)
+			//ori_traj[2][i] = (float)(i-350)*6.0/(50*3.14159);		
+	}
+	else if(i<600){
+		ori_traj[0][i] = 8.0+2.0+RR*sinf(2.0*Degg*(i-400)/200.0f-Degg);
+		ori_traj[1][i] = 1.5*(RR*cosf(2.0*Degg*(i-400)/200.0f-Degg)-1.0f/sinf(Degg));
+		ori_traj[2][i] = 0.0;//-((3.14159-2.0f*Degg)*(i-400)/200.0f+Degg-3.14159/2.0f);	
+	}
+	else if(i<1000){
+		ori_traj[0][i] = 12.0 + 0.0275*(i+1-600);
+		ori_traj[1][i] = 0.0;
 		ori_traj[2][i] = 0.0;	
+		//if (i<950)
+			//ori_traj[2][i] = (float)(i-950)*6.0/(50*3.14159);
 	}
 	else if(i<1500){
-		ori_traj[0][i] = 20-0.05 - 4.0*sinf(-(float)(i-1000)/500.0*3.14159/2.0);
+		ori_traj[0][i] = 20-0.05 - 4.0*sinf(-(float)(i-1000)/500.0*3.14159/2.0)+3;
 		ori_traj[1][i] = 4.0*cosf(-(float)(i-1000)/500.0*3.14159/2.0) - 4.0;
 		ori_traj[2][i] = -(float)(i-1000)/500.0*3.14159/2.0;
 	}
 	else{
-		ori_traj[0][i] = 24.0;
+		ori_traj[0][i] = 24.0+3;
 		ori_traj[1][i] = -4.0 - 0.02*(i-1500);
 		ori_traj[2][i] = -3.14159/2; 
 	}
@@ -361,6 +460,14 @@ int main(int argc, char **argv)
     
     ref_traj[0][SIM_TIME-1] = 0;
     ref_traj[1][SIM_TIME-1] = 0;
+
+
+    for (int i=0; i< SIM_TIME-1; i++){
+	for (int j=0; j<13; j++)
+		log_data[j][i] = 0;
+	for (int k=0; k<4; k++)
+		test_data[k][i] = 0;	
+    }	
 	
     carStartRefresh_ = false;
  
